a35e8de295b97954462ca02a79bea943202fce64
denkitdam <cryptoden145@gmail.com>
2023-03-23 19:57:46 +0300

    Created transition between 2 screens

--- NamesCompApp/FirstViewController.swift
+++ NamesCompApp/FirstViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet var yourNameTF: UITextField!
+    @IBOutlet var partnerNameTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let destinationVC = segue.destination as? ResultViewController else { return }
+        destinationVC.firstName = yourNameTF.text
+        destinationVC.secondName = partnerNameTF.text
     }
 
-
+    @IBAction func buttonWasTapped() {
+    }
 }
 
--- NamesCompApp/ResultViewController.swift
+++ NamesCompApp/ResultViewController.swift
@@ -8,10 +8,17 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-
+    
+    @IBOutlet var namesLabel: UILabel!
+    @IBOutlet var progressView: UIProgressView!
+    @IBOutlet var resultLabel: UILabel!
+    
+    var firstName: String!
+    var secondName: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         
     }
     
