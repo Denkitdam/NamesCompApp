67769c129be97adec11105741163a77e293b656f
denkitdam <cryptoden145@gmail.com>
2023-03-23 18:45:23 +0300

    ResultViewController created and connected with storyboard

---
+++ NamesCompApp/ResultViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ResultViewController.swift
+//  NamesCompApp
+//
+//  Created by Denis Kitaev on 23.03.2023.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+
+}
